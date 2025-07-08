c74b779defca312f21ddff43835ea964db3af33e
Vinicius Deolindo <andrade.vinicius934@gmail.com>
2025-07-08 15:49:02 -0300

    flake+ci: use flake to check and build

--- flake.nix
+++ flake.nix
@@ -22,16 +22,51 @@
         pkgs = import nixpkgs { inherit system overlays; };
 
         fenix-pkgs = import fenix { inherit system; };
+
         rust-toolchain = fenix-pkgs.fromToolchainFile {
           file = ./rust-toolchain.toml;
           sha256 = "sha256-KrfXTnUMDG3QBuAb2PQGrRigTdJep1omsY63UBRhCf8=";
         };
+
+        rustPlatform = pkgs.makeRustPlatform {
+          cargo = rust-toolchain;
+          rustc = rust-toolchain;
+        };
+
+        mkCheck =
+          name: command:
+          pkgs.stdenv.mkDerivation {
+            inherit name;
+            src = ./.;
+            dontBuild = true;
+            dontFixup = true;
+            doCheck = true;
+            nativeBuildInputs = [ rust-toolchain ];
+            checkPhase = command;
+            installPhase = ''mkdir "$out"'';
+          };
       in
       {
         devShells.default = pkgs.mkShell {
           packages = [
             rust-toolchain
           ];
+        };
+
+        checks = {
+          check = mkCheck "check" "cargo check";
+          lint = mkCheck "clippy" "cargo clippy -- -Dwarnings";
+          format = mkCheck "fmt" "cargo fmt --check";
+        };
+
+        packages = rec {
+          pom = rustPlatform.buildRustPackage {
+            name = "pom";
+            src = ./.;
+            cargoLock.lockFile = ./Cargo.lock;
+          };
+
+          default = pom;
         };
       }
     );
